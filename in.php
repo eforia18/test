b6fa568c7e3a308684a9157cbfeb4bde3dd93692
RavenAgtha18 <ravenagatha18@gmail.com>
2022-12-11 09:40:58 +0700

    file baru

---
+++ in.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Expedita facilis repudiandae quo similique, architecto nisi, nulla molestias assumenda quisquam, tenetur neque minima modi. Dolor commodi molestiae enim nemo magnam laborum!</p>
+    
+</body>
+</html>
